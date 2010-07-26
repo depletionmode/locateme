205f14f38a0a5d445aeed62f869cd21a2bcfabdd
David Kaplan <david@2of1.org>
2010-07-26 12:15:19 +0000

    Install function

--- locateme.php
+++ locateme.php
@@ -6,4 +6,18 @@
     Author URI: http://blog.2of1.org
     Version: 1.0
     */
+
+    function locateme_install()
+    {
+        global $wpdb;
+        $table = $wpdb->prefix."locateme_positions";
+        $structure = "CREATE TABLE $table (
+            id INT(9) NOT NULL AUTO_INCREMENT,
+            lon FLOAT,
+            lat FLOAT,
+            accuracy INT(9),
+            UNIQUE KEY id (id)
+        );";
+        $wpdb->query($structure);
+    }
 ?>
